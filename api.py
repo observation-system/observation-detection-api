6e1513f8deeb691193149749d3e0335a0a04c974
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-07 15:56:05 +0900

    Update api.py

--- api.py
+++ api.py
@@ -10,8 +10,8 @@ APP = FastAPI()
 YOLOV8_MODEL = YOLO("./yolo_models/yolov8n.pt")
 
 def check_directory():
-    detect_dir_path = "./detect_images"
-    runs_dir_path = "./runs"
+    detect_dir_path = "/detect_images"
+    runs_dir_path = "/runs"
 
     if os.path.isdir(detect_dir_path):
         shutil.rmtree(detect_dir_path)
@@ -28,7 +28,7 @@ def get_images(spot_data):
     for i in range(len(spot_data)):
         spot_key = spot_data[i]["spot_key"]
         url = spot_data[i]["url"]
-        dir_path = "./detect_images"
+        dir_path = "/detect_images"
         ext = "jpg"
         video = pafy.new(url)
         best = video.getbest(preftype="mp4")
@@ -42,7 +42,7 @@ def get_images(spot_data):
         ret, frame = cap.read()
         cv2.imwrite('{}.{}'.format(base_path, ext), frame)
 
-        results = YOLOV8_MODEL("./detect_images/%s.jpg" % spot_key ,save=True, classes=0)    
+        results = YOLOV8_MODEL("/detect_images/%s.jpg" % spot_key ,save=True, classes=0)    
         box_count = len(results[0].boxes.data)
         update_data.append({
             "spot_key": spot_key,
